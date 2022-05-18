31888654e091f9a295781e22bd1116813a4c8ebc
Ralphalien <105820907+Ralphalien@users.noreply.github.com>
2022-05-18 23:11:39 +0100

    Add files via upload

---
+++ aliens.py
@@ -0,0 +1,8 @@
+hours= input("enter hours:")
+rate= input("enter rate:")
+hours =int(hours)
+rate=int("rate")
+if hours != int(hours): 
+  print ("error, please enter numeric input")
+if rate is not int(rate):
+    print ("error, please enter numeric input")
---
+++ ex 2.py
@@ -0,0 +1,12 @@
+score= float(input("enter your score"))
+if score<0.6:
+    print ("F") 
+elif score>=0.6 and score<0.69:
+    print ("D ") 
+elif score>=0.7 and score<0.79:
+     print ("C")
+elif score>=0.8 and score<0.89:
+    print ("B")
+elif score >=0.9:
+    print ("D")
+#ex 3
